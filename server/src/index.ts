03798390f7e5764396751401bcc7b5ba1190c916
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-27 11:55:06 +0200

    database connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,13 +1,23 @@
+import 'dotenv/config';
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
-
-app.listen(port, () => {
-  console.log(`listening on port: ${port}`);
-});
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_DB_URI || '')
+  .then(() => {
+    console.log('Connected to Database');
+    app.listen(port, () => {
+      console.log(`listening on port: ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
