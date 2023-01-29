9b85139cf821461988ea46e1756aca66a123ed77
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-29 18:55:39 +0200

    Delete note

--- server/src/routes/notes.ts
+++ server/src/routes/notes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import {
   createNote,
+  deleteNote,
   getNote,
   getNotes,
+  updateNote,
 } from '../controllers/notesController';
 
 const router = express.Router();
@@ -10,5 +12,6 @@ const router = express.Router();
 router.get('/', getNotes);
 router.get('/:noteId', getNote);
 router.post('/', createNote);
-
+router.patch('/:noteId', updateNote);
+router.delete('/:noteId', deleteNote);
 export default router;
