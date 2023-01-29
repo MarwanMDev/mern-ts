9beb0392953988e98e49f2f89e0ef66b1d044dae
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-29 16:44:41 +0200

    Get notes from database

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,10 +1,36 @@
+import { NextFunction } from 'connect';
 import 'dotenv/config';
-
 import express from 'express';
+import NoteModel from './models/note';
+
 const app = express();
 
-app.get('/', (_req, res) => {
-  res.send('Hello World');
+app.get('/', async (_req, res, next) => {
+  try {
+    const notes = await NoteModel.find().exec();
+    res.status(200).json(notes);
+  } catch (error) {
+    next(error);
+  }
 });
+
+app.use((_req, _res, next) => {
+  next(Error('Endpoint not found.'));
+});
+
+app.use(
+  (
+    error: unknown,
+    _req: express.Request,
+    res: express.Response,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    next: NextFunction
+  ) => {
+    console.error(error);
+    let errMsg = 'error occurred while fetching notes';
+    if (error instanceof Error) errMsg = error.message;
+    res.status(500).json({ error: errMsg });
+  }
+);
 
 export default app;
