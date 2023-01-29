bf860dace7c493fb5f28fc4473c4a19a305f47cf
Marwan Mostafa <Marwan.morsi@outlook.com>
2023-01-29 19:47:28 +0200

    fetching notes to frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import { Note } from './models/note';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="text-red-500">Hello World</h1>
-    </div>
-  );
+  const [notes, setNotes] = useState<Note[]>([]);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    try {
+      const response = await fetch('/api/notes', {
+        method: 'GET',
+      });
+      const notes = await response.json();
+      console.log(notes);
+      setNotes(notes);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  return <div className="App">{JSON.stringify(notes)}</div>;
 }
 
 export default App;
